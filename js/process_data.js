b6971deab6ca079c785f4b9e5301b474e15abd5c
Your Name <zikitamar@gmail.com>
2020-04-13 12:26:27 -0400

    comment

--- js/process_data.js
+++ js/process_data.js
@@ -1,7 +1,7 @@
-//this is the data that is to be replaced by SUMBA data
+//initiate the array with the Sumba dictionary
 var trip_data = [{"lat" : -9.6993,
 "lon" : 119.9741,
-"name" : "Sumba",
+"name" : "Sumba Hospitality Foundation",
 country_code : 360,
 "flag" : "images/sumba.png",
 "guests" : 331,
@@ -9,9 +9,6 @@ country_code : 360,
   "NoStory" : ""
 }}]
 
-
-
-//var final_codes = []
 var final_guests = []
 var country_toCode = {}
 var country_toCoord = {}
@@ -23,8 +20,6 @@ var country_flags_loaded = false;
 // to import flags
 var country_flags = function() { d3.tsv("https://raw.githubusercontent.com/euctrl-pru/dashboard/master/data/world-country-flags.tsv").row(function(row) {
   country_toFlag[parseInt(row["id"])] = row["url"]
-  // console.log(parseInt(row["id"]))
-  // console.log(row)
   row.close = +row.close;
 }).get(function() {
   country_codes();
@@ -32,16 +27,12 @@ var country_flags = function() { d3.tsv("https://raw.githubusercontent.com/euctr
 })};
 
 
-
+//to bypass concurrency issues from loading data
 var country_codes_loaded = false;
 
+//load the country code dataset
 var country_codes = function() { d3.csv("/data/country_codes.csv").row(function(row) {
-  // country_toCode[row["name"]] = [row["country-code"], row["alpha-2"]]
   country_toCode[row["name"]] = [parseInt(row["country-code"])]
-  console.log(row["id"])
-
-  // console.log(country_toCode[row["short"]])
-  // console.log(country_toCode[row["name"]])
 
   row.close = +row.close;
 }).get(function() {
@@ -58,8 +49,6 @@ var geoCoord_data = function() {
 
   geo_cords_loaded = true;
   guest_data();
-  // console.log(final_codes)
-  // console.log(d)
 
 });
 };
@@ -67,20 +56,15 @@ var geoCoord_data = function() {
 var all_paths = []
 
 var country_to_guest_count = {}
-var bins;
-var color_options = ["#7CFC00", "#32CD32", "#006400"];
 
 var all_done = false;
+//initiate the final data load: importing the data of the guests given to us by partner org
 var guest_data = function() { d3.csv("/data/SHF_guests.csv").row(function(row) {
         var name = row["Nationality"]
         var count = parseInt(row['Number of Guests'])
         var code = country_toCode[row["Nationality"]]
         var flag = country_toFlag[code]
-        // console.log(country_toFlag[code])
-        // console.log(flag, code) *
-        // var short = country_toCode[row["Nationality"][1]]
         var coordinates = country_toCoord[row["Nationality"]]
-        // var flag = country_toFlag[row["url"]]
         if (name && count && code && coordinates){
           var dict = {
             name: name,
@@ -92,22 +76,17 @@ var guest_data = function() { d3.csv("/data/SHF_guests.csv").row(function(row) {
           country_to_guest_count[code] = count;
           return dict
 
-          // console.log(dict)
         };
-
-        //row.close = +row.close;
-
-        //2 10 18 24
-
 
 
   }).get(function(error, data) {
     final_guests = data;
-  //console.log("I SHOULD BE sECond!");
+
+//create array to fill in lines to Sumba
   for (i of final_guests) {
     all_paths.push(coordinates_to_sumba(i));
   }
-
+//create array of dictionaries to enable the flag logo
   for (i of final_guests) {
       var dict = {"lat" : i['coordinates'][1],
       "lon" : i['coordinates'][0],
@@ -125,60 +104,16 @@ var guest_data = function() { d3.csv("/data/SHF_guests.csv").row(function(row) {
 
 
 
-// histGenerator = d3.bin()
-//   .domain([0,130])
-//   .thresholds(3);
-//   console.log(total_guest_count);
-// foo = Object.values(country_to_guest_count).map(item => item.toString());
-// bins = histGenerator(foo);
 all_done = true;
+//load visualization once data imports are complete
 load_viz();
 }
 )};
 
 
-// var country_data = []
-//
-// for (i of guest_countries) {
-//   for (j of country_codes)
-//   if (i.Nationality == j.name)
-//   country_data.push({
-//     "name" : i.Nationality,
-//     "code" : j.countrycode,
-//     "guest_count" : i.NumberofGuests})
-//   }
-//
-
-//
-//
-//      in country_codes)
-// all_paths.push(coordinates_to_sumba(i))
-// console.log(i);
-//
-// function each country_codes, guest_countries
-
-
-
-
-
-
-
-
-
-
-
 //---------------------------------
 // to draw the Lines
 var sumba = [119.97, -9.69]
-var link =
-{type:
-  "LineString",
-  coordinates: [
-    [14.23, 15.74],
-    [23.51, -13.23]
-  ]
-}
-//dictionary from https://geojson-maps.ash.ms/
 
 
 function convert_country_to_long_lat(country_name) {
@@ -186,30 +121,11 @@ function convert_country_to_long_lat(country_name) {
 
 }
 
-// function coordinates_to_sumba(country_name) {
-//   console.log(convert_country_to_long_lat("fr"), sumba)
-//   return {type: "LineString", coordinates: [convert_country_to_long_lat(country_name), sumba]
-//   }
-
 
 function coordinates_to_sumba(country) {
   var coordinates = country['coordinates']
-  var name = country['name']
-  var is_european = (40 <= coordinates[1] && coordinates[1] <= 62)
-  && (-5 <= coordinates[0] && coordinates[0] <= 20)
-
-  var is_brazil = (-55 == coordinates[0]) && (-10 == coordinates[1])
-  var is_USA = (-97 == coordinates[0]) && (38 == coordinates[1])
-
-  // var is_asia = (40 <= country['coordinates'[1]] && country['coordinates'[1]] <= 62) && (-5 <= country['coordinates'[0]] && country['coordinates'[0]] <= 20)
-  // if (name = 'Belgium' || 'Germany' || 'Netherlands' || 'France' || 'Spain'
-  // || 'Poland' || 'Azerbaijan' || 'Italy' || 'Switzerland' || 'Luxembourg')
-  // var is_european = (40 <= coordinates[1] && coordinates[0]
-
-  // console.log(coordinates[1])
 
     return {type: "LineString", coordinates: [coordinates, sumba]
 
 }
 }
-//var all_paths = [];
--- js/visualization.js
+++ js/visualization.js
@@ -1,36 +1,32 @@
 
 var projection_outer
 var svg_outer;
+
+//loading the visualization after all external data has been collected
 window.onload = function() {
+//to bypass flag loading issue
 country_flags();
 
 }
+
+//define the boundaries of the map
 var legend_outer;
 var width = window.innerWidth,
-    height = window.innerHeight - 20,
-    centered,
-    clicked_point;
-var scale = 100
-var translate_width = width / 2
+    height = window.innerHeight
 var global_top;
-var translate_height = height / 2
 var load_viz = function() {
 d3.select("#map").selectAll("*").remove();
-//to do:
-//  - country labels
-//  - color gradient for country based on guest count
-//  - legend
-//  - map centered around Sumba island
 
-
-
+//set the earth projection from d3 geo library
 var projection = d3.geoMercator()
+.scale(width / 2 / Math.PI)
+      .translate([width / 2, height / 2])
 
 
-
+//create the svg object for the map
 var svg = d3.select("#map")
-    .attr("width", width)
-    .attr("height", height)
+    .attr("width", 1000)
+    .attr("height", 1000)
     .attr("class", "map");
 
 svg_outer = svg
@@ -38,82 +34,25 @@ svg_outer = svg
 var path = d3.geoPath()
     .projection(projection)
 
-    // var quantize = d3.scaleQuantize()
-    //   .domain([ 0, 150 ])
-    //   .range(d3.range(3).map(function(i) { return i; }));
-
 
 projection_outer = projection
-
-var plane_path = d3.geoPath()
-    	.projection(projection);
 
 var g = svg.append("g")
     .attr("width", "1000")
     .attr("height", "1000")
 
-
-    //returns the range of guest values
-
-
-
-    // svg.append("g")
-    //   .attr("class", "legendQuant")
-    //   .attr("transform", "translate(20,200)");
-
-    // var legend = d3.legendColor()
-    //   .labelFormat(d3.format("d"))
-    //   .useClass(true)
-    //   .title("Legend")
-    //   .titleWidth(100)
-  //   //   // .scale(quantize);
-  //
-  //     legend_outer = legend;
-  //
-  //     svg.select(".legendQuant")
-  // .call(legend);
-
-
-//
+// //create the tooltip for hovering
 var tooltip = d3.select("body")
   .append("div")
   .attr("class", "tooltip hidden");
 
-//this is for the legend that is to be impelented in sprint 2
-
-//
-// var legend  = d3.select(".legendContainer")
-//   				.append("ul")
-//   				.attr("class", "legend");
-
-          //add list item for every category
-
-
-
-
-var tooltip_point = d3.select("body")
-  	.append("div")
-  	.attr("class", "tooltip_point hidden");
-
-// for
-//
-//var extent = d3.extent(guest_scale);
-//
-
 sumba_coordinates = projection(sumba);
 
-
-// svg.append("image")
-//             .attr("x", sumba_coordinates[0])
-//             .attr("y", sumba_coordinates[1])
-//             .attr("width", 20)
-//             .attr("height", 20)
-//             .style("border-radius", "50%")
-//             .attr("href", "images/sumba.png");
-
+//function for making flag icons smaller in europe for clarity
+//increase the size of the Sumba logo
 var styleFlags = function(country) {
   // console.log(country);
-  if(country.name=="Sumba") {
+  if(country.name==="Sumba Hospitality Foundation") {
     return 40 + "px";
   }
 
@@ -129,47 +68,27 @@ var styleFlags = function(country) {
 
 }
 
-            var draw_flags = function() {
-                  	  // if zooming in -> draw points
-                  	  //if (centered !== null){
-                        		g.selectAll("image")
-                        		 .data(trip_data)
-                      		   .enter()
-                        		 .append("svg:image")
-                        		 .attr("xlink:href", function(d) {
-                               return d.flag
-                               // console.log(d.flag)
-                             }) //ADD SUMBA LOGO
-                        		 .attr("x", function(d) {
-                                    				return projection([d.lon - 2, d.lat])[0];
-                                            // console.log([d.lon, d.lat])
-                        		  })
-                        		 .attr("y", function(d) {
-                        				return projection([d.lon, d.lat])[1];
-                        		  })
+//draw all flags on the map
+  var draw_flags = function() {
+              		g.selectAll("image")
+              		 .data(trip_data)
+            		   .enter()
+              		 .append("svg:image")
+              		 .attr("xlink:href", function(d) {
+                     return d.flag
+                   }) //ADD SUMBA LOGO and flags in coordinates
+              		 .attr("x", function(d) {return projection([d.lon - 2, d.lat])[0];
+                                  // console.log([d.lon, d.lat])
+              		  })
+              		 .attr("y", function(d) {
+              				return projection([d.lon, d.lat])[1];
+              		  })
+              		 .attr("width", styleFlags)
+                   .attr("height",styleFlags)
+              		 .on("mousemove", showInfo)
+                   .on("mouseout", hideInfo)
 
-
-                        		 .attr("width", styleFlags)
-                               // console.log(d);
-                               // if(d.name=="Sumba") {
-                               //   return 40 + "px";
-                               // }
-                               // if ((-5 <= d.lon && d.lon <= 20)
-                               // && (40 <= d.lat && d.lat <= 62)) {
-                               //   console.log(d);
-                               //   console.log("THis countRy matCHES");
-                               //   return 10 + "px";
-                               // }
-                               // return 20 + "px";
-
-                             .attr("height",styleFlags)
-                        		 .on("mousemove", showTooltip)
-                             .on("mouseout", hideTooltip)
-                        		 ///.on("click", showTooltipPoint)
-                      //}
-
-
-            }
+  }
 // load and display the World
 d3.json("https://unpkg.com/world-atlas@1/world/50m.json", function(error, topology) {
   g.selectAll("path")
@@ -178,12 +97,11 @@ d3.json("https://unpkg.com/world-atlas@1/world/50m.json", function(error, topolo
         .enter()
         .append("path")
         .attr("d", path)
-        .on("click", clicked)
   	    .attr('fill', colorCountry)
-
         global_top = g
 
-        // Color the country when the legend is highligted
+        // Color the appropriate country when the legend is hovered over
+        //using jQuery to get the element of the legend itself to match countries within bounds
           $(".legend-labels").children().each(function() {
               var selectedLegendRangeElement = $(this);
 
@@ -202,10 +120,7 @@ d3.json("https://unpkg.com/world-atlas@1/world/50m.json", function(error, topolo
               }
 
               selectedLegendRangeElement.mouseover(function(d) {
-                //var people_bounds = d.target.innerHTML.split(" to ");
                 var matching_countries = g.selectAll('path').filter(function(d) {
-                  // console.log(parseInt(d.id));
-                  // console.log(parseInt(matching_country_codes));
                   return d.type==="Feature" && matching_country_codes.includes(parseInt(d.id))
 
 
@@ -221,51 +136,35 @@ d3.json("https://unpkg.com/world-atlas@1/world/50m.json", function(error, topolo
               })
 
           })
-        // .attr('fill',function(d,i) { return color(i); });
-        // set the color of each country
 
-        //create red lines to Sumba to display guest flow from all over the world
+        //create  lines to Sumba to display guest flow from all over the world
         g.selectAll("myPath")
             .data(all_paths)
             .enter()
             .append("path")
               .attr("d", function(d){return path(d)})
-              // .transition()
-              //   		//.style("stroke-width", (0.75 / k) + "px")
-              //   		.duration(750)
-              //   		.attr("transform", 2000)
               .attr('path', 5)
               .style("stroke", "blue")
               .style("stroke-opacity", 0.3)
               .style("fill", "none")
               .style("stroke-width", 1)
-
+        //draw the flags after we have created everything else
         draw_flags();
 
    });
 
- var visited_countries = ["360", "056", "840", "276", "528",
- 						 "250","724", "036", "124", "458",
- 						 "826", "104", "752", "031", "156",
- 						 "356", "380", "348", "410", "442",
- 						 "554", "702", "756", "376", "364",
-             "076", "616"];
 
-
-
+//count number of guests to create a simple chloropleth
 var code_tocount = {}
-
-// console.log(final_guests)
 
   for (i of final_guests) {
     code_tocount['country_code'] = i['country_code']
-    // console.log('hi')
   }
 
-
+//color options of the chloropleth
 var color_options = ['#abef95',"#8ae971", "#1fdb02","#268c16"];
 
-
+//color the country based on its number of guests
 function colorCountry(country) {
     var country_code = parseInt(country.id);
     var guests = country_to_guest_count[country_code]
@@ -278,7 +177,6 @@ function colorCountry(country) {
 
     if (11 <= guests && guests < 20) {
       return color_options[1]
-      // console.log(guests)
 
     }
 
@@ -297,135 +195,22 @@ function colorCountry(country) {
 
 };
 
-//this will be used in sprint 2...
-// color countries for particular legend item
 
-function colorCountriesCategory(d){
-    //filter relevant countries using hasContent function
-    var these_countries = trip_data.filter(function(s){
-                                                return hasContent(s, d.name);
-                                            });
-    var active_countries =  these_countries.map(function(a) {return a.country;});
-    var unique = active_countries.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
-
-    //color countries
-    g.selectAll('path')
-     .attr('fill', function(t){
-        return colorCountryLegend(t, unique);
-     });
-
-};
-
-
-	g.selectAll('path')
-	 .attr('fill', function(t){
-	 	return colorCountryLegend(t, unique);
-	 });
-
-
-// color country according to legend
-function colorCountryLegend(country, active_countries) {
-    if (active_countries.includes(country.id)) {
-        return '#f56260';
-    } else if (visited_countries.includes(country.id)) {
-        return '#c8b98d';
-    } else {
-    	return '#e7d8ad';
-    }
-};
-
-
-function hasContent(s, kind){
-
-	var post_keys = Object.keys(s.posts);
-    // console.log(post_keys.includes(kind));
-	return post_keys.includes(kind)
-};
-
-// get legend items
-function getLegend(d){
-    var temp = "placeholder";
-    temp = temp.replace("ICON_TITLE", d.name);
-    temp = temp.replace("ICON_LINK", d.url);
-    temp = temp.replace("ICON_KIND", d.name);
-    return(temp);
-};
-
-
-// var zoom = d3.zoom()
-//     .on("zoom",function() {
-//         var z = d3.event.transform;
-//         g.attr("transform", z);
-//         g.selectAll("path")
-//             .attr("d", path.projection(projection));
-//
-//         g.selectAll("circle")
-//          .attr("r", width / 100 / z.k);
-// });
-
-// // show country id on hover
-function showTooltipCountry(d){
-  var mouse = d3.mouse(svg.node()).map(function(d) {
-                        return parseInt(d);
-                    });
-  tooltip
-  .classed('hidden', false)
-  .html(d.id)
-  .attr('style',
-        'left:' + (mouse[0] + 15) + 'px; top:' + (mouse[1] - 35) + 'px')
-};
-
-// hide tooltip
-function hideTooltip(d) {
-  // Show the tooltip (unhide it) and set the name of the data entry.
+// hide the infobox
+function hideInfo(d) {
   tooltip
   .classed('hidden', true);
 }
-//
-// hide point tooltip
-function hideTooltipPoint(d) {
-  // Show the tooltip (unhide it) and set the name of the data entry.
-  tooltip_point
-  .classed('hidden', true);
-}
 
 
-// get icons to interact with link for more information regarding guests and country data
-function getIconsAndLinks(posts){
-    keys = Object.keys(posts["posts"]) //get available content
-    var st = "";
-
-    keys.forEach(function(key, index){
-        //if not story
-        if (key == "No Story"){
-            var this_img = "<p>Story coming soon...</p>" ;
-            st = this_img
-        }
-        // else {
-        //   //add image link
-        //     var this_img = "<a href='POST_LINK' target='_blank'><img class = 'icon' \
-        //                title='ICON_KIND' src='ICON_LINK' \
-        //                alt='' width='50' height='50' /></a>" ;
-        //     this_img = this_img.replace("POST_LINK", posts["posts"][key]);
-        //     this_img = this_img.replace("ICON_KIND", key);
-        //     this_img = this_img.replace("ICON_LINK", icon_links[key]);
-        //     st += this_img
-        // }
-    });
-    return st
-}
-
-
-// show location name on hover (still needs to be fixed)
-function showTooltip(d){
-  // console.log(d)
+// show name and guest count upon hover
+function showInfo(d){
   var mouse = d3.mouse(svg.node()).map(function(d) {
                         return parseInt(d);
                     });
-  if (tooltip_point.classed("hidden")){
   		tooltip
 	   .classed('hidden', false)
-	   .html("<span id='close' onclick='hideTooltipPoint()'>x</span>" +
+	   .html("<span id='close' onclick='hideInfo()'>x</span>" +
 	   "<div class='inner_tooltip'>" +
 	   			"<p>" + d.name + "</p>" +
         	 "</div><div>" + "Number of guests: " +
@@ -434,84 +219,8 @@ function showTooltip(d){
         	"</div>")
 	   .attr('style',
 			 'left:' + (mouse[0] + 15) + 'px; top:' + (mouse[1] - 35) + 'px')
-  };
 
 };
-
-// show point tooltip
-function showTooltipPoint(d){
-  var mouse = d3.mouse(svg.node()).map(function(d) {
-                        return parseInt(d);
-                    });
-  tooltip_point
-  .classed('hidden', false)
-  .html("<span id='close' onclick='hideTooltipPoint()'>x</span>" +
-	   "<div class='inner_tooltip'>" +
-	   			"<p>" + d.name + "</p>" +
-        	 "</div><div>" +
-        		// getIconsAndLinks(d) +
-    			//
-        	"</div>")
-  .attr('style',
-        'left:' + (mouse[0] + 15) + 'px; top:' + (mouse[1] - 35) + 'px')
-};
-
-
-//handle all clicking and zooming
-function clicked(d) {}
-  // console.log(d);
-  var d = 5
-  if (tooltip_point.classed("hidden")){
-      	  var x, y, k;
-
-      	  if ((d && centered !== d) & (visited_countries.includes(d.id))) {
-          	  	g.selectAll('path')
-          	 	  .attr('fill', colorCountry);
-
-            		var centroid = path.centroid(d);
-            		var bounds = path.bounds(d);
-            		var dx = bounds[1][0] - bounds[0][0],
-      			            dy = bounds[1][1] - bounds[0][1];
-
-      		      // legend_cont.classed("hidden", true);
-            		x = (bounds[0][0] + bounds[1][0]) / 2;
-            		y = (bounds[0][1] + bounds[1][1]) / 2;
-            		k = Math.min(width / dx, height / dy);
-            		centered = d;
-      	  }
-           else {
-            		x = width / 2;
-            		y = height / 2;
-            		k = 1;
-      		      centered = null;
-      		      //legend_cont.classed("hidden", false);
-      	  }
-
-      	  g.selectAll("path")
-      	   .classed("active", centered && function(d) { return d === centered; })
-
-
-      	  // make contours thinner before zoom
-
-
-      	  // map transition
-      	  // g.transition()
-          //   		//.style("stroke-width", (0.75 / k) + "px")
-          //   		.duration(750)
-          //   		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-          //   		.on('end', function() {
-          //         			if (centered === null){
-          //         			  g.selectAll("path")
-          //         			   .style("stroke-width", (0.75 / k) + "px");
-          // }
-          // 		  });
-
-      	  // remove all old points
-      	  g.selectAll("image").remove()
-
-}
-
-
 
 
 }
